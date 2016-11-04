006400fec2c0d1aed9c738bfac66fcabfecc2e8b
NiklasAkita <niklas137@hotmail.com>
2016-11-04 13:39:12 +0100

    Jedi message tester niklas

--- hotciv/src/hotciv/framework/City.java
+++ hotciv/src/hotciv/framework/City.java
@@ -28,6 +28,7 @@ Responsibilities:
    WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
    See the License for the specific language governing permissions and
    limitations under the License.
+    return of the jedi
 
 */
 public interface City {
